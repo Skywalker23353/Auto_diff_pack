9dc523464c9fadb895b3bbcf4ed454d2430c2bcf
Satyam Chauhan <satyamc@iisc.ac.in>
2025-12-02 09:44:00 +0530

    Implemented cold bc condition and added files

---
+++ AUTO_DIFF_PACK/chem_source_term_functions_cold_b.py
@@ -0,0 +1,66 @@
+import jax.numpy as jnp
+a_1 = 1.0
+a_2 = 1.0
+a = a_1 + a_2
+
+
+KinP = jnp.array([a_1,a_2],dtype=jnp.float64)
+R = 8.314 # J/molK
+
+def w_mol(rho, T, Y0, Y1, Y2, Y3, Y4, A, Ea, kappa, epsilon, W_k, T_u, T_c, delta):
+    """Compute the Q term for the given inputs.
+
+    Args:
+        rho (float): Density of the fluid.
+        T (float): Temperature of the fluid.
+        Y0, Y1, Y2, Y3, Y4): (float): Concentrations of different species.
+        A (float): Pre-exponential factor.
+        k (float): TKE.
+        epsilon (float): Dissipation.
+        W_k (array): Molecular weights of the species.
+    
+    Expresssion: A*exp(-Ta/T)*(rho*(Y_CH4/W_CH4))**a)*(rho*(Y_O2/W_O2))**b)
+
+    Returns:
+        float: Computed Q term.
+    """
+    # Compute Q term
+    rateConst  = A * jnp.exp(-Ea/(R*T))
+    w_mol_term = rateConst * ((rho*(Y0/W_k[0]))**KinP[0]) * ((rho*(Y1/W_k[1]))**KinP[1])
+    cold_b_param = 0.5 * ( 1 + jnp.tanh((T/T_u - T_c)/delta))
+    w_mol_term = w_mol_term * cold_b_param
+    return w_mol_term
+
+def omega_dot_CH4(rho, T, Y0, Y1, Y2, Y3, Y4, A, Ea, kappa, epsilon, W_k, nu_k, T_u, T_c, delta):
+    # Compute omega_dot for CH4
+    omega_dot = nu_k[0]*W_k[0]*w_mol(rho,T,Y0,Y1,Y2,Y3,Y4,A,Ea,kappa,epsilon,W_k, T_u, T_c, delta)
+    return omega_dot
+
+def omega_dot_O2(rho, T, Y0, Y1, Y2, Y3, Y4, A, Ea, kappa, epsilon, W_k, nu_k, T_u, T_c, delta):
+    # Compute omega_dot for O2
+    omega_dot = nu_k[1]*W_k[1]*w_mol(rho,T,Y0,Y1,Y2,Y3,Y4,A,Ea,kappa,epsilon,W_k, T_u, T_c, delta)
+    return omega_dot
+
+def omega_dot_CO2(rho, T, Y0, Y1, Y2, Y3, Y4, A, Ea, kappa, epsilon, W_k, nu_k, T_u, T_c, delta):
+    # Compute omega_dot for CO2
+    omega_dot = nu_k[2]*W_k[2]*w_mol(rho,T,Y0,Y1,Y2,Y3,Y4,A,Ea,kappa,epsilon,W_k, T_u, T_c, delta)
+    return omega_dot
+
+def omega_dot_H2O(rho, T, Y0, Y1, Y2, Y3, Y4, A, Ea, kappa, epsilon, W_k, nu_k, T_u, T_c, delta):
+    # Compute omega_dot for H2O
+    omega_dot = nu_k[3]*W_k[3]*w_mol(rho,T,Y0,Y1,Y2,Y3,Y4,A,Ea,kappa,epsilon,W_k, T_u, T_c, delta)
+    return omega_dot
+
+def omega_dot_N2(rho, T, Y0, Y1, Y2, Y3, Y4, A, Ea, kappa, epsilon, W_k, nu_k, T_u, T_c, delta):
+    # Compute omega_dot for N2
+    omega_dot = nu_k[4]*W_k[4]*w_mol(rho,T,Y0,Y1,Y2,Y3,Y4,A,Ea,kappa,epsilon,W_k, T_u, T_c, delta)
+    return omega_dot
+
+def omega_dot_T(rho, T, Y0, Y1, Y2, Y3, Y4, A, Ea, kappa, epsilon, W_k, nu_k, h_f, T_u, T_c, delta):
+    # Compute HRR differentials
+    omega_dot_T= -(h_f[0]*omega_dot_CH4(rho, T, Y0, Y1, Y2, Y3, Y4, A, Ea, kappa, epsilon, W_k, nu_k, T_u, T_c, delta) +
+                    h_f[1]*omega_dot_O2(rho, T, Y0, Y1, Y2, Y3, Y4, A, Ea, kappa, epsilon, W_k, nu_k, T_u, T_c, delta) +
+                    h_f[2]*omega_dot_CO2(rho, T, Y0, Y1, Y2, Y3, Y4, A, Ea, kappa, epsilon, W_k, nu_k, T_u, T_c, delta) +
+                    h_f[3]*omega_dot_H2O(rho, T, Y0, Y1, Y2, Y3, Y4, A, Ea, kappa, epsilon, W_k, nu_k, T_u, T_c, delta) +
+                    h_f[4]*omega_dot_N2(rho, T, Y0, Y1, Y2, Y3, Y4, A, Ea, kappa, epsilon, W_k, nu_k, T_u, T_c, delta))
+    return omega_dot_T
---
+++ compute_hrr_and_chem_species_diff_cold_b.py
@@ -0,0 +1,405 @@
+import jax.numpy as jnp
+import jax 
+import logging 
+from AUTO_DIFF_PACK.logging_util import get_logger,setup_logging
+from AUTO_DIFF_PACK import read_util as rfu
+from AUTO_DIFF_PACK import write_util as wfu
+from AUTO_DIFF_PACK import chem_source_term_functions_cold_b as cstf
+from AUTO_DIFF_PACK import reg_least_sq_fit as rlsf
+import os
+
+logger = get_logger()
+
+#Compute derivatives
+def main():
+    read_path = r"docs/FEHydro_P1"
+    #read_path = r"../.FEHydro_P1"
+    write_path = r"docs/Derivs_mod_ARR"
+    # write_path = r"../.FEHydro/Baseflow_CN_P1"
+    os.makedirs(write_path, exist_ok=True) 
+    
+    filename_Qbar = r"./Mean_Qbar.txt"
+    with open(filename_Qbar, 'r') as file:
+        Q_bar = jnp.array([float(file.readline().strip())], dtype=jnp.float64)
+
+    filename_A = r"./pre-exponential_A.txt"
+    with open(filename_A, 'r') as file:
+        A_arr = jnp.array([float(file.readline().strip())], dtype=jnp.float64)
+    # A = rfu.read_array_from_file(os.path.join(write_path ,'pre_exponential_field.txt'))
+
+    Ea_val = 31.588e3 # cal/mol
+    Ea_val = Ea_val*4.184 # J/mol
+
+    rho_ref = 1.0
+    T_ref = 800#K
+    l_ref = 2e-3#m
+    U_ref = 65#m/s
+    V_ref = l_ref**3
+    Cp_ref = 1100 #J/kg-K
+
+    # Species Data
+    h_f1 = -5.421277e06 #J/kg
+    h_f2 = 4.949450e05 #J/kg
+    h_f3 = -8.956200e06 #J/kg
+    h_f4 = -1.367883e07 #J/kg
+    h_f5 = 5.370115e05 #J/kg
+
+    W_k_CH4 = 16e-3 #kg/mol
+    W_k_O2 = 32e-3 #kg/mol
+    W_k_CO2 = 44e-3 #kg/mol
+    W_k_H2O = 18e-3 #kg/mol
+    W_k_N2 = 28e-3 #kg/mol
+
+    nu_k_CH4 = -1.0
+    nu_k_O2 = -2.0
+    nu_k_CO2 = 1.0
+    nu_k_H2O = 2.0
+    nu_k_N2 = 0.0
+    # W_k = jnp.array([16e-3,32e-3,44e-3,18e-3,28e-3],dtype=jnp.float64) #kg/mol
+    # nu_p_k = jnp.array([1.0,2.0,0.0,0.0,7.52],dtype=jnp.float64)
+    # nu_dp_k = jnp.array([0.0,0.0,1.0,2.0,7.52],dtype=jnp.float64)
+    # nu_k = nu_dp_k - nu_p_k
+ 
+    rhoM = rfu.read_array_from_file(os.path.join(read_path ,'rhobase.txt'))
+    rhoM = rho_ref * rhoM
+    TM = rfu.read_array_from_file(os.path.join(read_path ,'Tbase.txt'))
+    TM = T_ref * TM
+    Y1M = rfu.read_array_from_file(os.path.join(read_path ,'Ybase1.txt'))
+    Y2M = rfu.read_array_from_file(os.path.join(read_path ,'Ybase2.txt'))
+    Y3M = rfu.read_array_from_file(os.path.join(read_path ,'Ybase3.txt'))
+    Y4M = rfu.read_array_from_file(os.path.join(read_path ,'Ybase4.txt'))
+    Y5M = 1 - (Y1M + Y2M + Y3M + Y4M)
+
+    A = A_arr*jnp.ones(rhoM.shape, dtype=jnp.float64)
+    Ea = Ea_val*jnp.ones(rhoM.shape, dtype=jnp.float64)
+    T_u = T_ref * jnp.ones(rhoM.shape, dtype=jnp.float64)
+
+    
+    W_k_CH4_vec = W_k_CH4*jnp.ones(rhoM.shape, dtype=jnp.float64)
+    W_k_O2_vec = W_k_O2*jnp.ones(rhoM.shape, dtype=jnp.float64)
+    W_k_CO2_vec = W_k_CO2*jnp.ones(rhoM.shape, dtype=jnp.float64)
+    W_k_H2O_vec = W_k_H2O*jnp.ones(rhoM.shape, dtype=jnp.float64)
+    W_k_N2_vec = W_k_N2*jnp.ones(rhoM.shape, dtype=jnp.float64)
+    W_k = (W_k_CH4_vec, W_k_O2_vec, W_k_CO2_vec, W_k_H2O_vec, W_k_N2_vec) #tuple of arrays
+
+    nu_k_CH4_vec = nu_k_CH4*jnp.ones(rhoM.shape, dtype=jnp.float64)
+    nu_k_O2_vec = nu_k_O2*jnp.ones(rhoM.shape, dtype=jnp.float64)
+    nu_k_CO2_vec = nu_k_CO2*jnp.ones(rhoM.shape, dtype=jnp.float64)
+    nu_k_H2O_vec = nu_k_H2O*jnp.ones(rhoM.shape, dtype=jnp.float64)
+    nu_k_N2_vec = nu_k_N2*jnp.ones(rhoM.shape, dtype=jnp.float64)
+    nu_k = (nu_k_CH4_vec, nu_k_O2_vec, nu_k_CO2_vec, nu_k_H2O_vec, nu_k_N2_vec)
+
+    h_f1_vec = h_f1*jnp.ones(rhoM.shape, dtype=jnp.float64)
+    h_f2_vec = h_f2*jnp.ones(rhoM.shape, dtype=jnp.float64)
+    h_f3_vec = h_f3*jnp.ones(rhoM.shape, dtype=jnp.float64)
+    h_f4_vec = h_f4*jnp.ones(rhoM.shape, dtype=jnp.float64)
+    h_f5_vec = h_f5*jnp.ones(rhoM.shape, dtype=jnp.float64)
+    h_f = (h_f1_vec, h_f2_vec, h_f3_vec, h_f4_vec, h_f5_vec)
+    
+    if not os.path.exists(os.path.join(read_path, 'epsilon.txt')):
+        logger.info("Kappa and epsilon files not found. Setting the values to zero. \n")
+        kappa = jnp.zeros(rhoM.shape, dtype=jnp.float64)
+        epsilon = jnp.zeros(rhoM.shape, dtype=jnp.float64)
+    else:
+        kappa = rfu.read_array_from_file(os.path.join(read_path ,'TKE.txt')) #Turbulent Kinetic Energy
+        epsilon = rfu.read_array_from_file(os.path.join(read_path ,'epsilon.txt')) #Turbulent dissipation rate
+
+    # Read omega_dot_T_LES from file
+    omega_dot_T_LES = rfu.read_array_from_file(os.path.join(read_path, 'HRRbase.txt'))
+    omega_dot_T_LES_rms = rfu.read_array_from_file(os.path.join(read_path, 'HRRrms.txt'))
+    N_samples = 1160
+    
+    # Fit A and Ea using regularized least squares
+    model_uncty = jnp.array(0.01)
+    A_s_st = jnp.array(1.0)
+    Ea_s_st = jnp.array(1.0)
+    T_c_st = jnp.array(1.01)
+    delta_st = jnp.array(0.1)
+    st_params = jnp.array([A_s_st, Ea_s_st, model_uncty , T_c_st, delta_st], dtype=jnp.float64)
+    lambda_reg = jnp.array(10.0)
+    
+    A_s_opt, Ea_s_opt, T_c, delta = rlsf.fit_A_and_Ea(rhoM, TM, Y1M, Y2M, Y3M, Y4M, Y5M,
+                                  A_arr, Ea_val, W_k, nu_k, h_f, kappa, epsilon,
+                                   omega_dot_T_LES, omega_dot_T_LES_rms, N_samples, lambda_reg, st_params)
+    
+    logger.info("Completed fitting A and Ea.\n")
+    logger.info("A_s_opt: %.6e", A_s_opt)
+    logger.info("Ea_s_opt: %.6e", Ea_s_opt)
+
+    # Use optimized A and Ea for final calculations
+    A_arr_opt = A_s_opt * A_arr
+    Ea_val_opt = Ea_s_opt * Ea_val
+
+    logger.info("A_arr_opt: %.6e", A_arr_opt)
+    logger.info("Ea_arr_opt: %.6e", Ea_val_opt)
+    
+    del A 
+    del Ea
+
+    A = A_arr_opt*jnp.ones(rhoM.shape, dtype=jnp.float64)
+    Ea = Ea_val_opt*jnp.ones(rhoM.shape, dtype=jnp.float64)
+    T_c = T_c*jnp.ones(rhoM.shape, dtype=jnp.float64)
+    delta = delta*jnp.ones(rhoM.shape, dtype=jnp.float64)
+
+    species_idx = [1,2,3,4,5] #CH4, O2, CO2, H2O, N2
+    omega_dot_k_scaling = (rho_ref*U_ref)/l_ref
+    omega_dot_T_scaling = (rho_ref*Cp_ref*T_ref*U_ref)/l_ref
+
+    domega_dot_CH4_grad = jax.vmap(jax.grad(cstf.omega_dot_CH4, argnums=(0,1,2,3,4,5,6)))
+    domega_dot_O2_grad = jax.vmap(jax.grad(cstf.omega_dot_O2, argnums=(0,1,2,3,4,5,6)))
+    domega_dot_CO2_grad = jax.vmap(jax.grad(cstf.omega_dot_CO2, argnums=(0,1,2,3,4,5,6)))
+    domega_dot_H20_grad = jax.vmap(jax.grad(cstf.omega_dot_H2O, argnums=(0,1,2,3,4,5,6)))
+    domega_dot_N2_grad = jax.vmap(jax.grad(cstf.omega_dot_N2, argnums=(0,1,2,3,4,5,6)))
+
+    domega_dot_CH4_drho, domega_dot_CH4_dT, domega_dot_CH4_dY1, domega_dot_CH4_dY2, domega_dot_CH4_dY3, domega_dot_CH4_dY4, domega_dot_CH4_dY5 = domega_dot_CH4_grad(rhoM, TM, Y1M, Y2M, Y3M, Y4M, Y5M, A,  Ea, kappa, epsilon, W_k, nu_k, T_u, T_c, delta)
+    domega_dot_O2_drho, domega_dot_O2_dT, domega_dot_O2_dY1, domega_dot_O2_dY2, domega_dot_O2_dY3, domega_dot_O2_dY4, domega_dot_O2_dY5 = domega_dot_O2_grad(rhoM, TM, Y1M, Y2M, Y3M, Y4M, Y5M, A, Ea, kappa, epsilon, W_k, nu_k, T_u, T_c, delta)
+    domega_dot_CO2_drho, domega_dot_CO2_dT, domega_dot_CO2_dY1, domega_dot_CO2_dY2, domega_dot_CO2_dY3, domega_dot_CO2_dY4, domega_dot_CO2_dY5 = domega_dot_CO2_grad(rhoM, TM, Y1M, Y2M, Y3M, Y4M, Y5M, A, Ea, kappa, epsilon, W_k, nu_k, T_u, T_c, delta)
+    domega_dot_H2O_drho, domega_dot_H2O_dT, domega_dot_H2O_dY1, domega_dot_H2O_dY2, domega_dot_H2O_dY3, domega_dot_H2O_dY4, domega_dot_H2O_dY5 = domega_dot_H20_grad(rhoM, TM, Y1M, Y2M, Y3M, Y4M, Y5M, A, Ea, kappa, epsilon, W_k, nu_k, T_u, T_c, delta)
+    domega_dot_N2_drho, domega_dot_N2_dT, domega_dot_N2_dY1, domega_dot_N2_dY2, domega_dot_N2_dY3, domega_dot_N2_dY4, domega_dot_N2_dY5 = domega_dot_N2_grad(rhoM, TM, Y1M, Y2M, Y3M, Y4M, Y5M, A, Ea, kappa, epsilon, W_k, nu_k, T_u, T_c, delta)
+    domega_dot_T_grad = jax.vmap(jax.grad(cstf.omega_dot_T, argnums=(0,1,2,3,4,5,6)))
+    domega_dot_T_drho, domega_dot_T_dT, domega_dot_T_dY1, domega_dot_T_dY2, domega_dot_T_dY3, domega_dot_T_dY4, domega_dot_T_dY5 = domega_dot_T_grad(rhoM, TM, Y1M, Y2M, Y3M, Y4M, Y5M, A, Ea, kappa, epsilon, W_k, nu_k, h_f, T_u, T_c, delta)
+
+    # Compute domega_dot_k_drho_terms---------------------------------------------------
+    domega_dot_CH4_drho_s = (domega_dot_CH4_drho*rho_ref)/(omega_dot_k_scaling) 
+    wfu.write_to_file(write_path, "domega_dot_" + str(species_idx[0]) + "_drho", domega_dot_CH4_drho_s)
+    del domega_dot_CH4_drho_s
+
+    domega_dot_O2_drho_s = (domega_dot_O2_drho*rho_ref)/(omega_dot_k_scaling)
+    wfu.write_to_file(write_path, "domega_dot_" + str(species_idx[1]) + "_drho", domega_dot_O2_drho_s)
+    del domega_dot_O2_drho_s
+
+    domega_dot_CO2_drho_s = (domega_dot_CO2_drho*rho_ref)/(omega_dot_k_scaling)
+    wfu.write_to_file(write_path, "domega_dot_" + str(species_idx[2]) + "_drho", domega_dot_CO2_drho_s)
+    del domega_dot_CO2_drho_s
+
+    domega_dot_H2O_drho_s = (domega_dot_H2O_drho*rho_ref)/(omega_dot_k_scaling)
+    wfu.write_to_file(write_path, "domega_dot_" + str(species_idx[3]) + "_drho", domega_dot_H2O_drho_s)
+    del domega_dot_H2O_drho_s
+
+    domega_dot_N2_drho_s = (domega_dot_N2_drho*rho_ref)/(omega_dot_k_scaling)
+    wfu.write_to_file(write_path, "domega_dot_" + str(species_idx[4]) + "_drho", domega_dot_N2_drho_s)
+    del domega_dot_N2_drho_s
+    #Compute domega_dot_T_drho---------------------------------------------------------
+    cn_rho = domega_dot_T_drho * rho_ref * V_ref / Q_bar
+    domega_dot_T_drho = (domega_dot_T_drho*rho_ref)/(omega_dot_T_scaling)
+    
+    wfu.write_to_file(write_path, "domega_dot_T_drho", domega_dot_T_drho)
+    wfu.write_to_file(write_path, "CN_rho", cn_rho)
+    del cn_rho
+    del domega_dot_T_drho
+    del domega_dot_CH4_drho 
+    del domega_dot_O2_drho
+    del domega_dot_CO2_drho
+    del domega_dot_H2O_drho 
+    del domega_dot_N2_drho
+    #Compute domega_dot_k_dT_terms---------------------------------------------------
+    domega_dot_CH4_dT_s = (domega_dot_CH4_dT*T_ref)/(omega_dot_k_scaling)  
+    wfu.write_to_file(write_path, "domega_dot_" + str(species_idx[0]) + "_dT", domega_dot_CH4_dT_s)
+    del domega_dot_CH4_dT_s
+    
+    domega_dot_O2_dT_s = (domega_dot_O2_dT*T_ref)/(omega_dot_k_scaling)
+    wfu.write_to_file(write_path, "domega_dot_" + str(species_idx[1]) + "_dT", domega_dot_O2_dT_s)
+    del domega_dot_O2_dT_s
+    
+    domega_dot_CO2_dT_s = (domega_dot_CO2_dT*T_ref)/(omega_dot_k_scaling)
+    wfu.write_to_file(write_path, "domega_dot_" + str(species_idx[2]) + "_dT", domega_dot_CO2_dT_s)
+    del domega_dot_CO2_dT_s
+
+    domega_dot_H2O_dT_s = (domega_dot_H2O_dT*T_ref)/(omega_dot_k_scaling)
+    wfu.write_to_file(write_path, "domega_dot_" + str(species_idx[3]) + "_dT", domega_dot_H2O_dT_s)
+    del domega_dot_H2O_dT_s
+
+    domega_dot_N2_dT_s = (domega_dot_N2_dT*T_ref)/(omega_dot_k_scaling)
+    wfu.write_to_file(write_path, "domega_dot_" + str(species_idx[4]) + "_dT", domega_dot_N2_dT_s)
+    del domega_dot_N2_dT_s
+    #Compute domega_dot_T_dT---------------------------------------------------------
+    cn_T = (domega_dot_T_dT * V_ref * T_ref) / Q_bar
+    domega_dot_T_dT = (domega_dot_T_dT*T_ref)/(omega_dot_T_scaling)
+    
+    wfu.write_to_file(write_path, "domega_dot_T_dT", domega_dot_T_dT)
+    wfu.write_to_file(write_path, "CN_T", cn_T)
+    del cn_T
+    del domega_dot_T_dT
+    del domega_dot_CH4_dT
+    del domega_dot_O2_dT
+    del domega_dot_CO2_dT
+    del domega_dot_H2O_dT
+    del domega_dot_N2_dT
+    #Compute domega_dot_k_dY1_terms---------------------------------------------------
+    domega_dot_CH4_dY1_s = (domega_dot_CH4_dY1)/(omega_dot_k_scaling)   
+    wfu.write_to_file(write_path, "domega_dot_" + str(species_idx[0]) + "_dY1", domega_dot_CH4_dY1_s)
+    del domega_dot_CH4_dY1_s
+    
+    domega_dot_O2_dY1_s = (domega_dot_O2_dY1)/(omega_dot_k_scaling)
+    wfu.write_to_file(write_path, "domega_dot_" + str(species_idx[1]) + "_dY1", domega_dot_O2_dY1_s)
+    del domega_dot_O2_dY1_s
+    
+    domega_dot_CO2_dY1_s = (domega_dot_CO2_dY1)/(omega_dot_k_scaling)
+    wfu.write_to_file(write_path, "domega_dot_" + str(species_idx[2]) + "_dY1", domega_dot_CO2_dY1_s)
+    del domega_dot_CO2_dY1_s
+    
+    domega_dot_H2O_dY1_s = (domega_dot_H2O_dY1)/(omega_dot_k_scaling)
+    wfu.write_to_file(write_path, "domega_dot_" + str(species_idx[3]) + "_dY1", domega_dot_H2O_dY1_s)
+    del domega_dot_H2O_dY1_s
+    
+    domega_dot_N2_dY1_s = (domega_dot_N2_dY1)/(omega_dot_k_scaling)
+    wfu.write_to_file(write_path, "domega_dot_" + str(species_idx[4]) + "_dY1", domega_dot_N2_dY1_s)
+    del domega_dot_N2_dY1_s
+    #Compute domega_dot_T_dY1---------------------------------------------------------
+    cn_Y1 = domega_dot_T_dY1 * V_ref / Q_bar
+    domega_dot_T_dY1 = (domega_dot_T_dY1)/(omega_dot_T_scaling)
+    
+    wfu.write_to_file(write_path, "domega_dot_T_dY1", domega_dot_T_dY1)
+    wfu.write_to_file(write_path, "CN_Y1", cn_Y1)
+    del cn_Y1
+    del domega_dot_T_dY1
+    del domega_dot_CH4_dY1
+    del domega_dot_O2_dY1
+    del domega_dot_CO2_dY1
+    del domega_dot_H2O_dY1
+    del domega_dot_N2_dY1
+    #Compute domega_dot_k_dY2_terms---------------------------------------------------
+    domega_dot_CH4_dY2_s = (domega_dot_CH4_dY2)/(omega_dot_k_scaling)  
+    wfu.write_to_file(write_path, "domega_dot_" + str(species_idx[0]) + "_dY2", domega_dot_CH4_dY2_s)
+    del domega_dot_CH4_dY2_s
+
+    domega_dot_O2_dY2_s = (domega_dot_O2_dY2)/(omega_dot_k_scaling)
+    wfu.write_to_file(write_path, "domega_dot_" + str(species_idx[1]) + "_dY2", domega_dot_O2_dY2_s)
+    del domega_dot_O2_dY2_s
+    
+    domega_dot_CO2_dY2_s = (domega_dot_CO2_dY2)/(omega_dot_k_scaling)
+    wfu.write_to_file(write_path, "domega_dot_" + str(species_idx[2]) + "_dY2", domega_dot_CO2_dY2_s)
+    del domega_dot_CO2_dY2_s
+    
+    domega_dot_H2O_dY2_s = (domega_dot_H2O_dY2)/(omega_dot_k_scaling)
+    wfu.write_to_file(write_path, "domega_dot_" + str(species_idx[3]) + "_dY2", domega_dot_H2O_dY2_s)
+    del domega_dot_H2O_dY2_s
+
+    domega_dot_N2_dY2_s = (domega_dot_N2_dY2)/(omega_dot_k_scaling)
+    wfu.write_to_file(write_path, "domega_dot_" + str(species_idx[4]) + "_dY2", domega_dot_N2_dY2_s)
+    del domega_dot_N2_dY2_s
+    #Compute domega_dot_T_dY2---------------------------------------------------------
+    cn_Y2 = domega_dot_T_dY2 * V_ref / Q_bar
+    domega_dot_T_dY2 = (domega_dot_T_dY2)/(omega_dot_T_scaling)
+    
+    wfu.write_to_file(write_path, "domega_dot_T_dY2", domega_dot_T_dY2)
+    wfu.write_to_file(write_path, "CN_Y2", cn_Y2)
+    del cn_Y2
+    del domega_dot_T_dY2
+    del domega_dot_CH4_dY2
+    del domega_dot_O2_dY2
+    del domega_dot_CO2_dY2
+    del domega_dot_H2O_dY2
+    del domega_dot_N2_dY2
+    #Compute domega_dot_k_dY3_terms---------------------------------------------------
+    domega_dot_CH4_dY3_s = (domega_dot_CH4_dY3)/(omega_dot_k_scaling)
+    wfu.write_to_file(write_path, "domega_dot_" + str(species_idx[0]) + "_dY3", domega_dot_CH4_dY3_s)
+    del domega_dot_CH4_dY3_s
+
+    domega_dot_O2_dY3_s = (domega_dot_O2_dY3)/(omega_dot_k_scaling)
+    wfu.write_to_file(write_path, "domega_dot_" + str(species_idx[1]) + "_dY3", domega_dot_O2_dY3_s)
+    del domega_dot_O2_dY3_s
+
+    domega_dot_CO2_dY3_s = (domega_dot_CO2_dY3)/(omega_dot_k_scaling)
+    wfu.write_to_file(write_path, "domega_dot_" + str(species_idx[2]) + "_dY3", domega_dot_CO2_dY3_s)
+    del domega_dot_CO2_dY3_s
+
+    domega_dot_H2O_dY3_s = (domega_dot_H2O_dY3)/(omega_dot_k_scaling)
+    wfu.write_to_file(write_path, "domega_dot_" + str(species_idx[3]) + "_dY3", domega_dot_H2O_dY3_s)
+    del domega_dot_H2O_dY3_s
+
+    domega_dot_N2_dY3_s = (domega_dot_N2_dY3)/(omega_dot_k_scaling)
+    wfu.write_to_file(write_path, "domega_dot_" + str(species_idx[4]) + "_dY3", domega_dot_N2_dY3_s)
+    del domega_dot_N2_dY3_s
+    #Compute domega_dot_T_dY3---------------------------------------------------------
+    cn_Y3 = domega_dot_T_dY3 * V_ref / Q_bar
+    domega_dot_T_dY3 = (domega_dot_T_dY3)/(omega_dot_T_scaling)
+    
+    wfu.write_to_file(write_path, "domega_dot_T_dY3", domega_dot_T_dY3)
+    wfu.write_to_file(write_path, "CN_Y3", cn_Y3)
+    del cn_Y3
+    del domega_dot_T_dY3
+    del domega_dot_CH4_dY3
+    del domega_dot_O2_dY3
+    del domega_dot_CO2_dY3
+    del domega_dot_H2O_dY3
+    del domega_dot_N2_dY3
+    #Compute domega_dot_k_dY4_terms---------------------------------------------------
+    domega_dot_CH4_dY4_s = (domega_dot_CH4_dY4)/(omega_dot_k_scaling)
+    wfu.write_to_file(write_path, "domega_dot_" + str(species_idx[0]) + "_dY4", domega_dot_CH4_dY4_s)
+    del domega_dot_CH4_dY4_s
+
+    
+    domega_dot_O2_dY4_s = (domega_dot_O2_dY4)/(omega_dot_k_scaling)
+    wfu.write_to_file(write_path, "domega_dot_" + str(species_idx[1]) + "_dY4", domega_dot_O2_dY4_s)
+    del domega_dot_O2_dY4_s
+
+    domega_dot_CO2_dY4_s = (domega_dot_CO2_dY4)/(omega_dot_k_scaling)
+    wfu.write_to_file(write_path, "domega_dot_" + str(species_idx[2]) + "_dY4", domega_dot_CO2_dY4_s)
+    del domega_dot_CO2_dY4_s
+
+    domega_dot_H2O_dY4_s = (domega_dot_H2O_dY4)/(omega_dot_k_scaling)
+    wfu.write_to_file(write_path, "domega_dot_" + str(species_idx[3]) + "_dY4", domega_dot_H2O_dY4_s)
+    del domega_dot_H2O_dY4_s
+
+    domega_dot_N2_dY4_s = (domega_dot_N2_dY4)/(omega_dot_k_scaling)
+    wfu.write_to_file(write_path, "domega_dot_" + str(species_idx[4]) + "_dY4", domega_dot_N2_dY4_s)
+    del domega_dot_N2_dY4_s
+    #Compute domega_dot_T_dY4---------------------------------------------------------
+    cn_Y4 = domega_dot_T_dY4 * V_ref / Q_bar
+    domega_dot_T_dY4 = (domega_dot_T_dY4)/(omega_dot_T_scaling)
+    
+    wfu.write_to_file(write_path, "domega_dot_T_dY4", domega_dot_T_dY4)
+    wfu.write_to_file(write_path, "CN_Y4", cn_Y4)
+    del cn_Y4
+    del domega_dot_T_dY4
+    del domega_dot_CH4_dY4
+    del domega_dot_O2_dY4
+    del domega_dot_CO2_dY4
+    del domega_dot_H2O_dY4
+    del domega_dot_N2_dY4
+    #Compute domega_dot_k_dY5_terms---------------------------------------------------
+    domega_dot_CH4_dY5_s = (domega_dot_CH4_dY5)/(omega_dot_k_scaling)
+    wfu.write_to_file(write_path, "domega_dot_" + str(species_idx[0]) + "_dY5", domega_dot_CH4_dY5_s)
+    del domega_dot_CH4_dY5_s
+
+    domega_dot_O2_dY5_s = (domega_dot_O2_dY5)/(omega_dot_k_scaling)
+    wfu.write_to_file(write_path, "domega_dot_" + str(species_idx[1]) + "_dY5", domega_dot_O2_dY5_s)
+    del domega_dot_O2_dY5_s
+
+    domega_dot_CO2_dY5_s = (domega_dot_CO2_dY5)/(omega_dot_k_scaling)
+    wfu.write_to_file(write_path, "domega_dot_" + str(species_idx[2]) + "_dY5", domega_dot_CO2_dY5_s)
+    del domega_dot_CO2_dY5_s
+
+    domega_dot_H2O_dY5_s = (domega_dot_H2O_dY5)/(omega_dot_k_scaling)
+    wfu.write_to_file(write_path, "domega_dot_" + str(species_idx[3]) + "_dY5", domega_dot_H2O_dY5_s)
+    del domega_dot_H2O_dY5_s
+
+    domega_dot_N2_dY5_s = (domega_dot_N2_dY5)/(omega_dot_k_scaling)
+    wfu.write_to_file(write_path, "domega_dot_" + str(species_idx[4]) + "_dY5", domega_dot_N2_dY5_s)
+    del domega_dot_N2_dY5_s
+    #Compute domega_dot_T_dY5---------------------------------------------------------
+    cn_Y5 = domega_dot_T_dY5 * V_ref / Q_bar
+    domega_dot_T_dY5 = (domega_dot_T_dY5)/(omega_dot_T_scaling)
+    
+    wfu.write_to_file(write_path, "domega_dot_T_dY5", domega_dot_T_dY5)
+    wfu.write_to_file(write_path, "CN_Y5", cn_Y5)
+    del cn_Y5
+    del domega_dot_T_dY5
+    del domega_dot_CH4_dY5
+    del domega_dot_O2_dY5
+    del domega_dot_CO2_dY5
+    del domega_dot_H2O_dY5
+    del domega_dot_N2_dY5
+
+    logger.info("\nDONE\n")
+if __name__ == "__main__":
+    script_directory = os.path.dirname(os.path.abspath(__file__))
+    setup_logging(level=logging.DEBUG, script_dir=script_directory)
+    try:
+        main()
+        logger.info("Script completed successfully")
+    except Exception as e:
+        logger.error("An error occurred: %s", str(e), exc_info=True)
+        raise
+    
